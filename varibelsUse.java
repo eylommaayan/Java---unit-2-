7df2c8816af0421318c73e98226c44d34c414ad5
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-03-22 17:17:47 +0200

    הוספת קבוע לפקודת הדפסה

--- varibelsUse.java
+++ varibelsUse.java
@@ -1,8 +1,12 @@
 public class varibelsUse {
+    static final int MIN = 1; // defining the MIN constant with the value 1
+
     public static void main(String[] args) {
         int x = 10;
         double d = 5.12;
         boolean b = true;
+
+        System.out.println("Value of MIN: " + MIN); // printing the value of the MIN constant
 
         System.out.println("Value of x: " + x);
         System.out.println("Value of d: " + d);
