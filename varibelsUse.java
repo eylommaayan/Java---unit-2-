bc63a94c59402337f0e66a8bf573d17a0189e6aa
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-03-22 17:16:55 +0200

    varibelsUse

---
+++ varibelsUse.java
@@ -0,0 +1,11 @@
+public class varibelsUse {
+    public static void main(String[] args) {
+        int x = 10;
+        double d = 5.12;
+        boolean b = true;
+
+        System.out.println("Value of x: " + x);
+        System.out.println("Value of d: " + d);
+        System.out.println("Value of b: " + b);
+    }
+}
