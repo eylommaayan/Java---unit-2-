10eb07fb6e1a6aa1785c1ac659f8010742d23554
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-04-06 11:54:19 +0300

    תוכנית שבודקת אם המספר שהכונס הוא יחיד או לא

--- IfElse_SingleOrDigitNumber.java
+++ IfElse_SingleOrDigitNumber.java
@@ -1,7 +1,21 @@
+// תוכנית שבודקת אם המספר שהכונס הוא יחיד או לא 
+
+import java.util.Scanner;
+
 public class IfElse_SingleOrDigitNumber {
     
     public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter any  number:");
+        int number = scanner.nextInt();
 
+        if (number > 9 ) {
+            System.out.println("This is digit number");
+        }
+        else {
+            System.out.println("This is singl number");
+        }
     }
 
 }
