2c96a6a2d45109f3ad9ce56ba1782a7880c1c107
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-04-08 17:14:32 +0300

    code

--- If_UpperHeight.java
+++ If_UpperHeight.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class UpperHeight {
+public class If_UpperHeight {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
